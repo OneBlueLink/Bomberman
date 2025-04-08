522706235162065b0acb6f3fddf71396709c65da
Danieve00 <daniela.rosales@saei.mx>
2025-04-08 17:37:03 -0600

    Taller 1

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    InputSystem_Actions actions;
+    
+
+    public Vector2 moveDir;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+        actions = new InputSystem_Actions();
+        actions.Enable();
+        actions.Player.Move.performed += i => moveDir = i.ReadValue<Vector2>();
+        actions.Player.Move.canceled += i => moveDir = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    
+
+}
